1f5971f668765731295cb001aa0add0046a83daa
Utku Yaşar <48256126+utkuyasar7@users.noreply.github.com>
2023-08-18 20:20:24 +0300

    Update ipinfoHelper.js

--- utils/ipinfoHelper.js
+++ utils/ipinfoHelper.js
@@ -1,24 +1,28 @@
-const apiUrl = "https://ipinfo.io"
+const fetch = require('node-fetch');
 
-function getIPDetail(ip) {
-    const url = `${apiUrl}/${ip}/json`;
-  
-    const promise = fetch(url)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`HTTP Error! Status: ${response.status}`);
-        }
-        return response.json();
-      });
-  
-       return{
-        ip : promise.ip,
-        city : promise.city,
-        country : promise.country,
-        org : promise.org
-    } 
+const apiUrl = "https://ipinfo.io";
+
+async function getIPDetail(ip) {
+  const url = `${apiUrl}/${ip}/json`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP Error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+
+    return {
+      ip: data.ip,
+      city: data.city,
+      country: data.country,
+      org: data.org
+    };
+  } catch (error) {
+    throw error;
   }
-  
-  module.exports = {
-    getIPDetail
-  };
+}
+
+module.exports = {
+  getIPDetail
+};
