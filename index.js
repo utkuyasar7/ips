ea2579f90c852b669b24721e52e224965718d452
Utku Yaşar <48256126+utkuyasar7@users.noreply.github.com>
2023-08-18 20:18:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get("/sa", async (req, res) => {
 
   try {
     const ipDetails = await ipInfo.getIPDetail(clientIP);
-    res.send(ipDetails);
+    res.json(ipDetails); // Cevabı JSON formatında gönderiyoruz
   } catch (error) {
     console.error('Hata:', error);
     res.status(500).send('Bir hata oluştu.');
