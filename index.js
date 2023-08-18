cd155dfe080d63bfbf6efa0934e183b945f36ac5
Utku Yaşar <48256126+utkuyasar7@users.noreply.github.com>
2023-08-18 20:13:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const app = express();
-const ipInfo = require("./utils/ipinfoHelper")
+const ipInfo = require("./utils/ipinfoHelper");
 app.enable('trust proxy');
 
-app.get("/sa", (req, res) => {
+app.get("/sa", async (req, res) => {
   const clientIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
-
-  res.send( ipInfo.getIPDetail(clientIP));
+  try {
+    const ipDetails = await ipInfo.getIPDetail(clientIP);
+    res.send(ipDetails);
+  } catch (error) {
+    console.error('Hata:', error);
+    res.status(500).send('Bir hata oluştu.');
+  }
 });
 
 const PORT = process.env.PORT || 3000;
