73fc4c916da8fd4714abe2a612214d8b576776ad
Utku Yaşar <48256126+utkuyasar7@users.noreply.github.com>
2023-08-18 19:39:52 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.enable('trust proxy');
+
+app.get("/", (req, res) => {
+  const clientIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+  res.send(`Client IP: ${clientIP}`);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
