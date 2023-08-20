12ad5b8c24ab56d875e2a68268226383621d65c7
Utku Yaşar <48256126+utkuyasar7@users.noreply.github.com>
2023-08-20 17:36:19 +0300

    Update all.js

--- routes/all.js
+++ routes/all.js
@@ -3,18 +3,17 @@ const router = express.Router();
 const cors = require('cors');
 const domainController = require('../controllers/index');
 
-// CORS ayarlarını özelleştirme
 const corsOptions = {
-  origin: '*', // Tüm originlerden gelen isteklere izin verilir
+  origin: '*', 
   methods: 'GET,POST',
   optionsSuccessStatus: 200
 };
 router.use(cors(corsOptions));
 
-// /resolve rotası
+
 router.post('/resolve', domainController.searchDomain);
 
-// /ip rotası
+
 router.get('/ip', domainController.IPController);
 
 module.exports = router;
