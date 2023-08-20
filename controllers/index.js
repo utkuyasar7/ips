9e6780e78788dbfac42dfaf30a8e6cded7623c11
Utku Yaşar <48256126+utkuyasar7@users.noreply.github.com>
2023-08-20 13:47:25 +0300

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -24,7 +24,7 @@ async function searchDomain(req, res) {
 
  
 
-    res.json({ domain,ipDetails});
+    res.json({ipDetails});
   } catch (error) {
     console.error('Hata:', error);
     res.status(500).send("hata");
