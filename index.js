d89af1b074ef82b9f5674aa05d1f2415a82953c6
Utku Yaşar <48256126+utkuyasar7@users.noreply.github.com>
2023-08-20 14:50:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser'); // Gelen istek verilerini analiz etmek için
-const domainRoutes = require('./routes/all'); // domain rotalarını içeren dosya
+const bodyParser = require('body-parser'); 
+const domainRoutes = require('./routes/all'); 
 const cors = require('cors');
-app.enable('trust proxy');
-app.use(bodyParser.json()); // JSON verileri için middleware
+
+app.use(bodyParser.json()); 
 app.use(cors());
+
 // Ana sayfa rotası
 app.get('/', (req, res) => {
   res.send('Merhaba, Express uygulamasına hoş geldiniz!');
